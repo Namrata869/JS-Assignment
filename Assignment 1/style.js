e657d289a629b7fe979afc8c2b8f64e3ce3c7753
Namrata869 <namratarajput9027@gmail.com>
2026-06-22 14:56:36 +0530

    Add files via upload

---
+++ Assignment 1/style.js
@@ -0,0 +1,52 @@
+// Q1
+// let A = 2;
+// let B = 5;
+
+// let sum = A + B;
+
+// console.log(sum);
+
+// Q2
+// let A = 5;
+// let B = 3;
+
+// console.log(A < 10 && A > B);
+
+// Q3
+// let A = 12;
+// let B = 20;
+
+// console.log(A % 10 === 0 || B % 10 === 0);
+
+// Q4
+// let N = 4567;
+
+// console.log(parseInt(N / 1000));
+
+
+// Q5
+// let N = 4567;
+
+// console.log(N % 10);
+
+// Q6
+// let A = 2;
+// let B = 9;
+
+// console.log(B % A);
+
+// Q7
+// let A = 2;
+// let B = 5;
+
+// console.log(A * B);
+
+let A = 50;
+let B = 20;
+let C = 100;
+
+let total = A + B + C;
+let average = total / 3;
+
+console.log(total);
+console.log(average);
